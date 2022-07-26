1def1c5e2216e8f894fd4e333a15478731773492
Trong Bao <trongbao0208@gmail.com>
2022-07-26 18:46:50 -0500

    7/26/2022

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
-// npm - global command, comes with node
-// npm --version
+var http = require('http')
+var fs = require('fs')
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
-
-// global dependency - use it in any project
-//npm install -g <packageName>
-// sudo install -g <packageName> (mac)
-
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create package.json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-const _ = require('lodash')
-
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems);
+http
+    .createServer((req, res) => {
+        // const text = fs.readFileSync('./content/big.txt', 'utf8')
+        // res.end(text)
+        const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+        fileStream.on('open', () =>{
+            fileStream.pipe(res);
+        })
+        fileStream.on('error', (err) => {
+            res.end(err)
+        })
+    })
+    .listen(5000)
